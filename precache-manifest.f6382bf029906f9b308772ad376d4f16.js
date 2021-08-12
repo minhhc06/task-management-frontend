7b4309534800fdfac832c44c34a85ba355ca2f05
minhhc06 <minhhc06@users.noreply.github.com>
2021-08-12 07:19:33 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.f6382bf029906f9b308772ad376d4f16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a4450f255f2f29b658908009371e6d",
+    "url": "https://minhhc06.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3f01537551a42d32949c",
+    "url": "https://minhhc06.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0493c9025f29e7fc3cce",
+    "url": "https://minhhc06.github.io/task-management-frontend/static/js/2.cad290d6.chunk.js"
+  },
+  {
+    "revision": "3f01537551a42d32949c",
+    "url": "https://minhhc06.github.io/task-management-frontend/static/js/main.1978c306.chunk.js"
+  },
+  {
+    "revision": "d61ab4402fcb1b52a07f",
+    "url": "https://minhhc06.github.io/task-management-frontend/static/js/runtime~main.862e16d2.js"
+  }
+]);
